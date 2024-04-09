04fba549ee2df727c7b9e088f7d9f287f9546bad
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-04-09 18:40:36 +0545

    modified:   greatest_num_among_three.class

--- greatest_num_among_three.java
+++ greatest_num_among_three.java
@@ -1,18 +1,17 @@
 public class greatest_num_among_three {
     public static void main(String[] args) {
-        int a = 50, b = 20, c = 15;
+        int a = 50, b = 60, c = 75;
         // int max = Math.max(a, Math.max(b, c));
         // System.out.println(max);
-        if (a>b) {
-            if (a>c) {
-                System.out.println("a is greatest " + a);
+        if (a>b && a>c){
+            System.out.println("a is greatest " + a);
             }
-            else {
-                System.out.println("c is greatest " + c);
-            }
-        }
-        else {
+        else if (b>c){
                 System.out.println("b is greatest " + b);
+            }
+        
+        else {
+                System.out.println("c is greatest " + c);
         }
     }
 }
