16c721fe29200c976597e9922776c7b3614f2731
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-04-09 18:13:20 +0545

    file added

---
+++ conditional.java
@@ -0,0 +1,10 @@
+public class conditional {
+    public static void main(String[] args) {
+        int age = 15;
+        if (age >= 18) {
+            System.out.println("Eligible to vote");
+        } else {
+            System.out.println("Not eligible to vote");
+        }
+    }
+}
