dba9c633b068de2e55baeb63e559f9c83b13bb76
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-04-09 14:22:01 +0545

    relational_operator_file

---
+++ relationaloperator.java
@@ -0,0 +1,11 @@
+public class relationaloperator {
+    public static void main(String[] args) {
+        // >, <, ==, !=, <=, >=, 
+        int x = 6; 
+        int y = 5;
+        boolean result = x<y;
+        boolean result1 = x>y;
+        System.out.println(result);
+        System.out.println(result1);
+    }
+}
