ba81ac6ecc88d621eab27139e7e0fe49e37f8acf
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-04-09 09:30:21 +0545

    modified

---
+++ variables.java
@@ -0,0 +1,9 @@
+public class variables {
+    public static void main(String[] args) {
+        int num = 3; //integervariable
+        System.out.println(num);
+        int num1 = 10;
+        int num2 = 20;
+        System.out.println(num1 + num2);
+    }
+}
