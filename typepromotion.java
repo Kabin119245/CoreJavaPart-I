f369e644d996978ba689562db03e1174bbaa1e0d
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-04-09 11:24:00 +0545

    promotion_file_added

---
+++ typepromotion.java
@@ -0,0 +1,9 @@
+public class typepromotion {
+    public static void main(String[] args) {
+        byte a = 10;
+        byte b = 30;
+        int result = a*b; //promotion
+        System.out.println(result);
+        
+    }
+}
