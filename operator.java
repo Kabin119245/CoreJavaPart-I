d868a579d40daeaef0feaae6b2b32519b24e5c3a
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-04-09 14:14:09 +0545

    operator_file

---
+++ operator.java
@@ -0,0 +1,18 @@
+public class operator {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+        int res1 = num1 - num2;
+        int res2 = num1 * num2;
+        int res3 = num1 / num2;
+        int res4 = num1 % num2;
+        System.out.println(res1 + " " + res2 + " " + res3 + " " + res4);
+        int n = 5;
+        n += 5; // n = n + 5;
+        //n++ post increment
+        // ++n; pre increment
+        //n-- or --n
+        System.out.println(n);
+        
+    }
+}
