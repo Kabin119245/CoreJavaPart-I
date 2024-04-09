1aa8e723baa0abf6e6f8aa700753acf58b9ad21a
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-04-09 09:56:15 +0545

    added

---
+++ varables_one.java
@@ -0,0 +1,12 @@
+public class varables_one {
+    public static void main(String[] args) {
+        int num1 = 9;
+        byte by = 78; 
+        short sh = 559;
+        long l = 99999999l;
+        float f = 99.99f; 
+        boolean b = true;
+        char c = 'A';
+        System.out.println("num1: " + num1 + ", by: " + by + ", sh: " + sh + ", l: " + l + ", f: " + f + ", b: " + b + ", c: " + c);
+    }
+}
