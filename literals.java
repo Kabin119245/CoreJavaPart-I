0281669735c9a2190b02cc3a837603f1f98b420c
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-04-09 10:16:56 +0545

    literals_added

---
+++ literals.java
@@ -0,0 +1,14 @@
+public class literals {
+    public static void main(String[] args) {
+        int num1 = 0b101; //binary
+        int num2 = 0x1011; //hex
+        double num3 = 12e10;
+        char c = 'a';
+        c++;
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(c);
+
+    }
+}
