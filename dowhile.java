3369c9012ce26ad8b8f30c1c44539622416b656e
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-04-10 17:28:47 +0545

    dowhile: file added

---
+++ dowhile.java
@@ -0,0 +1,13 @@
+public class dowhile {
+    public static void main(String[] args) {
+        //execute the statement even if the condition does not satifies
+
+             int i = 1;
+        do {
+                System.out.println("Hello World");
+                i++;
+
+
+        }while(i<5);
+    }
+}
