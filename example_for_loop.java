1ff0451f9072a582d1d4668fd46f9b647f73b927
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-04-10 17:50:13 +0545

    file added

---
+++ example_for_loop.java
@@ -0,0 +1,12 @@
+public class example_for_loop {
+    public  static void main (String[] args) {
+        for (int i = 1; i <= 7; i++) {
+            System.out.println("DAY " + i  );
+            for (int j = 1; j <= 9; j++) {
+                System.out.println("    " + (j+8) + " - " + (j +9) +" " + "TASK: "  );
+
+            }
+        }
+
+    }
+}
