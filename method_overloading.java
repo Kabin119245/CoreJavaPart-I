4df0a63c6760a64ea6a5b16fccfc53294da722ab
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-04-10 23:46:38 +0545

    file added

---
+++ method_overloading.java
@@ -0,0 +1,29 @@
+class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public double add(double a, int b) {
+        return a + b;
+    }
+
+    public int add(int a,int b, int c){
+        return a + b + c;
+    }
+    
+}
+
+
+public class method_overloading {
+    public static void main(String[] args) {
+
+        Calculator calc = new Calculator();
+        double r1 = calc.add(5.3, 2);
+        System.out.println(r1);
+        int r2 = calc.add(5,2);
+        System.out.println(r2);
+        int r3 = calc.add(5, 2, 3);
+        System.out.println(r3);
+        
+    }
+}
