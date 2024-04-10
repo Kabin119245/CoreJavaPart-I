92dda2e00364dcd875bdc013fe853a0fc09ff19e
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-04-10 18:13:45 +0545

    file added

---
+++ pattern_one.java
@@ -0,0 +1,14 @@
+public class pattern_one {
+    public static void main(String[] args) {
+        //right traingle pattern
+        pattern(5);
+    }
+    static void pattern(int n) {
+        for (int i = 1; i <= n; i++){
+            for (int j = 1; j < i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+} 
