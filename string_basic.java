6757466dbb21375653f4d0dbf8b5fa6da7239397
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-04-11 19:11:16 +0545

    file added

---
+++ string_basic.java
@@ -0,0 +1,7 @@
+public class string_basic {
+    public static void main(String[] args) {
+        //String name = new String("Kabin");
+        String name = "Kabin";
+        System.out.println("Hello, " + name);
+    }
+}
