e4b9c3627e7538224f620698799087706d062902
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-04-11 13:35:54 +0545

    updated

--- primenumber.java
+++ primenumber.java
@@ -1,3 +1,20 @@
 public class primenumber {
-    
+
+    public static void main(String[] args) {
+        int n = 7;
+        int count = 0;
+        for (int i = 1; i < n; i ++) {
+            if (n % i == 0) {
+                count ++;
+            }
+            
+        }
+        if (count == 1) {
+            System.out.println(n + " is a prime number");
+        }
+        else {
+            System.out.println(n + " is not a prime number");
+        }
+    }
 }
+    
