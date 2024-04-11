536d084ff8ba2d5b93eca590929e1f30c95f589c
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-04-11 12:47:50 +0545

    added

---
+++ array_two.java
@@ -0,0 +1,8 @@
+public class array_two {
+    public static void main(String[] args) {
+        int nums[] = {1, 2, 3, 4, 5};
+        //changing elememt 
+        nums[2] = 100;
+        System.out.println(nums[2]);
+    }
+}
