9c7be408ec546b65f8315259faca001e307d4fdf
Kabin_giri <kabin.077bct043@acem.edu.np>
2024-04-14 19:39:14 +0545

    updated

--- this_super.java
+++ this_super.java
@@ -1,7 +1,7 @@
 //every constructor in java has a method which is there even if you dont mention it and that method is called Super
 
 
-class A { // A is super class 
+class A  extends Object{ // A is super class , even if you dont mention extends Object it is there
     public A() {
         super();
         System.out.println("In A");
@@ -15,7 +15,12 @@ class A { // A is super class
 
 }
 
+//here we can see that suoer method of class B is trivial as it has super class A but what about class A, as it doesnt have a super class 
+//so the answer to that question is every class in java extends Object class
+
 class B extends A { // B is subclass
+    //Class B extends A and A extends Object class--> multilevel inheritance
+
     public B() {
         super(); //John Cena method, because you can't see it, super means call the constructor of super class
         System.out.println("In B");
