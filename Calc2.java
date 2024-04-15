de6a88c39ca36242b13d2434081272774d8b82c1
Kabin_giri <kabin.077bct043@acem.edu.np>
2024-04-15 11:44:41 +0545

    file added

---
+++ Calc2.java
@@ -0,0 +1,12 @@
+public class Calc2 extends Calc1{
+
+    public int multi(int n1, int n2) {
+        return n1 * n2;
+    }
+    
+
+    public int div(int n1, int n2) {
+        return n1 / n2;
+    }
+    
+}
