2e2795381534b3ceece3c1ca4577499f78c18daa
Kabin_giri <kabin.077bct043@acem.edu.np>
2024-04-15 12:13:41 +0545

    multilevel -added

--- CalcMain.java
+++ CalcMain.java
@@ -1,11 +1,13 @@
 public class CalcMain {
     public static void main(String[] args) {
-        Calc2 obj = new Calc2();
+        Calc3 obj = new Calc3();
         int r1 = obj.add(8, 11);
         int r2 = obj.sub(10, 5);
         int r3 = obj.multi(10, 5);
         int r4 = obj.div(10, 5);
+        double r5 = obj.power(2, 3);
+        int r6 = obj.factorial(5);
 
-        System.out.println(r1 + " "+ r2  +" "+ r3 + " " + r4);
+        System.out.println(r1 + " "+ r2  +" "+ r3 + " " + r4 + " " + r5 + " " + r6);
     }
 }
